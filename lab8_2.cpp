877c08e656c6cef35b246b58370856117639552d
Seacil04 <everest1756@gmail.com>
2024-12-06 15:34:25 +0700

    Missing Code kub

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double x){
+    return ((x > 90) ? 'A' : (x > 75 and x <= 90) ? 'B': (x > 60 and x <= 75) ? 'C': (x > 45 and x <=60) ? 'D' : 'F');
+}
 
 int main(){
 	//Input the number of students
@@ -11,15 +14,17 @@ int main(){
 	cout << "Enter the number of students: ";
 	cin >> N;
 	string name[N];
-	float score[N];	
+	double score[N];	
 	
 	//Store names and scores of students into an array 
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
